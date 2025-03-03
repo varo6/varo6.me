4f01269bd3a47c6451a39cf3529e21152f4219e4
varose <alvarohr2003@gmail.com>
2025-03-03 11:39:48 +0000

    about page and api endpoints

--- src/api.rs
+++ src/api.rs
@@ -2,25 +2,40 @@ use askama::Template;
 use axum::response::Html;
 use axum::{routing::get, Router};
 
-pub async fn about() -> Html<&'static str> {
-    Html("<h1> About </h1>
-        <p>Aquí se irán subiendo los proyectos, de momento está en construcción. 🚧</p>")
+#[derive(Template)]
+#[template(path = "about.html")]
+pub struct AboutTemplate {}
+
+pub async fn about() -> Result<Html<String>, axum::http::StatusCode> {
+    let template = AboutTemplate {};
+    template
+        .render()
+        .map(Html)
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)
 }
 
+#[derive(Template)]
+#[template(path = "projects.html")]
+pub struct ProjectsTemplate {}
 
+pub async fn projects() -> Result<Html<String>, axum::http::StatusCode> {
+    let template = ProjectsTemplate {};
+    template
+        .render()
+        .map(Html)
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)
+}
 
-// Versión con template (comentada)
-// pub async fn hello_htmx_template() -> Result<Html<String>, axum::http::StatusCode> {
-//     let template = HelloFragment {};
-//     template
-//         .render()
-//         .map(Html)
-//         .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)
-// }
+#[derive(Template)]
+#[template(path = "workflow.html")]
+pub struct WorkflowTemplate {}
 
-pub async fn projects() -> Html<&'static str> {
-    Html("<h1> Proyectos </h1>
-        <p>Aquí se irán subiendo los proyectos, de momento está en construcción. 🚧</p>")
+pub async fn workflow() -> Result<Html<String>, axum::http::StatusCode> {
+    let template = WorkflowTemplate {};
+    template
+        .render()
+        .map(Html)
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)
 }
 
 // Función para configurar las rutas API
@@ -28,4 +43,5 @@ pub fn routes() -> Router {
     Router::new()
         .route("/api/projects", get(projects))
         .route("/api/about", get(about))
+        .route("/api/workflow", get(workflow))
 }
